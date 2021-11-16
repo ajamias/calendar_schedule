be52ef104c0e44528123d42c0b4b7bf4a20b1a00
Austin <ajamias@bu.edu>
2021-11-16 13:04:13 -0500

    commit on 'main'

--- calendar_project.c
+++ calendar_project.c
@@ -2,8 +2,8 @@
 
 typedef struct{
     char name[10];
-    int block_amount;
-    char start_times[20][7], end_times[20][7];
+    int block_amount, start_min[20], start_hr[20], end_min[20], end_hr[20];
+    char start_ampm[20], end_ampm[20];
 } person;
 
 int main(){
@@ -23,27 +23,28 @@ int main(){
 
     for (i=0;i<p1.block_amount;i++){
         printf("Enter start time busy: ");
-        scanf("%s", p1.start_times[i]);
+        scanf("%i:%i%c", &p1.start_hr[i], &p1.start_min[i], &p1.start_ampm[i]);
         printf("Enter end time busy: ");
-        scanf("%s", p1.end_times[i]);
+        scanf("%i:%i%c", &p1.end_hr[i], &p1.end_min[i], &p1.end_ampm[i]);
     }
-    
+    /*
         for (i=0;i<p2.block_amount;i++){
         printf("Enter start time busy: ");
         scanf("%s", *p2.start_times);
         printf("Enter end time busy: ");
         scanf("%s", *p2.end_times);
     }
-    
+    */
     for (i=0;i<p1.block_amount;i++){
-        printf("%s\n", p1.start_times[i]);
-        printf("%s\n", p1.end_times[i]);
+        printf("%i:%i%c\n", p1.start_hr[i], p1.start_min[i], p1.start_ampm[i]);
+        printf("%i:%i%c\n", p1.end_hr[i], p1.end_min[i], p1.end_ampm[i]);
     }
+    /*
     for (i=0;i<p2.block_amount;i++){
     printf("%s\n", p2.start_times[i]);
     printf("%s\n", p2.end_times[i]);
-}
-
+    }
+    */
 
     return 0;
 }
